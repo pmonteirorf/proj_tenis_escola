da4e95f57121790b82d2abafdd3fcb16fec8fcd3
Priscilla Monteiro <priiscilla.monteiro@gmail.com>
2024-03-02 14:59:36 +0800

    Anuario 2021 e 2023 / Pg Contatos
    Criação da timeline do ano de 2023; adicionando textos no anuario 2021; alterações na página de contatos;

--- forms/contact.php
+++ forms/contact.php
@@ -1,13 +1,7 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'priiscilla.monteiro@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,14 +18,12 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
+    'host' => 'br466.hostgator.com',
+    'username' => 'contato@tenisescola.org',
     'password' => 'pass',
-    'port' => '587'
+    'port' => '25'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
